22c44545e493c20829e87910c35f6c3fa7f7c708
unknown <seba13102004@gmail.com>
2020-07-22 01:33:55 -0300

    main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ BLANCO = (255, 255, 255)
 
 pygame.init()
 
-# Se definen las dimenciones de la pantalla.
+# Se definen las dimensiones de la pantalla.
 dimensiones = [700,500]
 
 # Se define la pantalla con los parametros previamente definidos.
@@ -22,6 +22,12 @@ hecho = False
 # esta esta multiplicada por la escala en pixeles.
 gravedad = 6.674 * 10
 
+# Se define el tamaño de la bola.
+tamaño = 15
+
+# Se define el grosor de la bola, este no puede se mayor que el tamaño.
+grosor = 15
+
 # Se definen la altura de los objetos. Posicion inicial igual a 50px.
 posicion_y = 50
 
@@ -32,7 +38,7 @@ posicion_x = 150
 inercia = [0, 14]
 
 # Se define la masa que ba a tener el objeto.
-masa = 70
+masa = 50
 
 # Se define los eventos del bucle principal.
 event = pygame.event.wait()
@@ -58,36 +64,36 @@ while not hecho:
         inercia[0] += (gravedad / masa)
 
     # Si el objeto se encuentra antes del borde inferior de la pantalla, se agrega la inercia vertical a su altura.
-    if posicion_y < 490:
+    if posicion_y < (dimensiones[1] - tamaño):
         posicion_y += round(inercia[0])
 
     #Si el objeto se encuntra mas abajo o en el mismo lugar del borde inferior de la pantallla...
-    elif posicion_y >= 490:
+    elif posicion_y >= (dimensiones[1] - tamaño):
 
         # Si la inercdia vertical del objeto es mayor a 0, esta se multiplica por -1,
         # se le agrega un quinto de la masa del objeto y se lleva al borde de la pantalla.
         if round(inercia[0]) > 0:
             inercia[0] *= -1
             inercia[0] += masa / 5
-            posicion_y = 490
+            posicion_y = (dimensiones[1] - tamaño)
 
         # Si la inercia vertical del objeto es menor a 0, se le agrega la inercia vertical a la altura.
         if inercia[0] < 0:
             posicion_y += round(inercia[0])
 
     # Si el objeto se pasa del borde inferior de la pantalla, es llevado de vuelta a este.
-    if posicion_y > 490:
-        posicion_y = 490
+    if posicion_y > (dimensiones[1] - tamaño):
+        posicion_y = (dimensiones[1] - tamaño)
 
     #Si el objeto se encuntra mas abajo o en el mismo lugar del borde superior de la pantallla...
-    if posicion_y <= 10:
+    if posicion_y <= tamaño:
 
         # Si la inercdia vertical del objeto es menor a 0, esta se multiplica por -1,
         # se le agrega un decimo de la masa del objeto y se lleva al borde de la pantalla.
         if round(inercia[0]) < 0:
             inercia[0] *= -1
             inercia[0] += masa / 10
-            posicion_y = 10
+            posicion_y = tamaño
 
         # Si la inercia vertical del objeto es mayor a 0, se le agrega la inercia vertical a la altura.
         if inercia[0] > 0:
@@ -95,11 +101,11 @@ while not hecho:
 
     # Si el objeto esta dentro de los bordes laterales se le agrega a su posicion horizontal su
     # inercia Horizontal.
-    if posicion_x < 690 and posicion_x > 0:
+    if posicion_x < (dimensiones[0] - tamaño) and posicion_x > 0:
         posicion_x += round(inercia[1])
 
     #Si el objeto se encuntra mas alla o en el mismo lugar del borde derecho de la pantallla...
-    elif posicion_x >= 690:
+    elif posicion_x >= (dimensiones[0] - tamaño):
 
         # Si la inercia horizontal del objeto es menor a 0, se le agrega la inercia horizontal
         # a la posicion horizontal.
@@ -111,10 +117,10 @@ while not hecho:
         if round(inercia[1]) > 0:
             inercia[1] *= -1
             inercia[1] += masa / 10
-            posicion_x = 690
+            posicion_x = (dimensiones[0] - tamaño)
 
     #Si el objeto se encuntra mas alla o en el mismo lugar del borde izquierdo de la pantallla...
-    if posicion_x <= 10:
+    if posicion_x <= tamaño:
 
         # Si la inercia horizontal del objeto es menor a 0, se le agrega la inercia horizontal
         # a la posicion horizontal.
@@ -126,7 +132,7 @@ while not hecho:
         if round(inercia[1]) < 0:
             inercia[1] *= -1
             inercia[1] -= masa / 10
-            posicion_x = 10
+            posicion_x = tamaño
 
 
     # --- EL CÓDIGO DE DIBUJO DEBERÍA IR AQUÍ
@@ -136,7 +142,7 @@ while not hecho:
     pantalla.fill(BLANCO)
 
     # A continuacion se dibuja el obeto.
-    pygame.draw.circle(pantalla, NEGRO, (posicion_x, posicion_y), 10, 10)
+    pygame.draw.circle(pantalla, NEGRO, (posicion_x, posicion_y), tamaño, grosor)
 
 
     # --- Avanzamos y actualizamos la pantalla con lo que hemos dibujado.
